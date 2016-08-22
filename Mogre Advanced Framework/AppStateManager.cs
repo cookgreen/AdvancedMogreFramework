e21073454649f7bb684085d1a2e85884b6c4464c
Cook Green <2721595960@qq.com>
2016-08-22 12:48:49 +0800

    Update Code
    Fix some proble

--- Mogre Advanced Framework/AppStateManager.cs
+++ Mogre Advanced Framework/AppStateManager.cs
@@ -142,7 +142,7 @@ namespace Mogre_Advanced_Framework
              if (m_ActiveStateStack.Count != 0)
              {
                  m_ActiveStateStack.Last().exit();
-                 m_ActiveStateStack.RemoveAt(m_ActiveStateStack.Count());
+                 m_ActiveStateStack.RemoveAt(m_ActiveStateStack.Count()-1);
              }
 
              if (m_ActiveStateStack.Count != 0)
