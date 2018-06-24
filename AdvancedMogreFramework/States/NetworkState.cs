2e46340a74eaeedd15d32e4c2d90148dccdb13fd
Cook Green <cookgreen@users.noreply.github.com>
2018-06-24 20:44:30 +0800

    Change Build Directory

---
+++ AdvancedMogreFramework/States/NetworkState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedMogreFramework.States
+{
+    class NetworkState : AppState
+    {
+        public override void enter()
+        {
+            base.enter();
+        }
+
+        public override void exit()
+        {
+            base.exit();
+        }
+
+        public override void update(double timeSinceLastFrame)
+        {
+            base.update(timeSinceLastFrame);
+        }
+    }
+}
